3298bcd54c83a867b0f8bbd52c130fec0a92ef71
Sejoslaw <k.dobrzynski94@gmail.com>
2025-02-09 16:21:40 +0100

    Initial commit

---
+++ src/client/java/com/github/sejoslaw/multiversecraft/client/MultiverseCraftClient.java
@@ -0,0 +1,9 @@
+package com.github.sejoslaw.multiversecraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MultiverseCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/github/sejoslaw/multiversecraft/MultiverseCraft.java
@@ -0,0 +1,9 @@
+package com.github.sejoslaw.multiversecraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MultiverseCraft implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
